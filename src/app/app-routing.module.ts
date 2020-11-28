6ab101eb8c9d15f3bfa3c427293e78caea231eb3
poojatodkar1407 <poojatodkar124@gmail.com>
2020-11-28 13:26:17 +0530

    Fronted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+const routes: Routes = [
+  { path:'employee',component:EmployeeComponent},
+  { path:'create',component:CreateEmployeeComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { HttpClientModule, /* other http imports */ } from "@angular/common/http";
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    CreateEmployeeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
